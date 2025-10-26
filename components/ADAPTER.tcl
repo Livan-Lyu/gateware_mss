f3228eddaaa520a3c888721ce40f4431824fe838
Lars Randers <lranders@mail.dk>
2025-10-26 17:22:48 +0100

    Swap SPI_1 and QSPI connections
    - Adjust abstraction layer
    - Move devices on the device-tree
    
    Signed-off-by: Lars Randers <lranders@mail.dk>

--- components/ADAPTER.tcl
+++ components/ADAPTER.tcl
@@ -1,4 +1,4 @@
-puts "======== Adapter option: QSPI ========"
+puts "======== Adapter option: DEFAULT ========"
 
 namespace eval adapter {
 
@@ -10,27 +10,25 @@ namespace eval adapter {
             "SPI_1_SS1" {
                 sd_delete_ports -sd_name ${sd_name} -port_names {SPI_1_SS1}
                 sd_delete_instances -sd_name ${sd_name} -instance_names {SPI_1_SS1_BIBUF}
-                sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_SS_F2M} -value {GND}
-                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_SS1_M2F}
-                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_SS1_OE_M2F}
+                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_SEL_M2F}
+                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_SEL_OE_M2F}
             }
             "SPI_1_CLK" {
                 sd_delete_ports -sd_name ${sd_name} -port_names {SPI_1_CLK}
                 sd_delete_instances -sd_name ${sd_name} -instance_names {SPI_1_CLK_BIBUF}
-                sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_CLK_F2M} -value {GND}
-                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_CLK_M2F}
-                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_CLK_OE_M2F}
+                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_CLK_M2F}
+                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_CLK_OE_M2F}
             }
             "SPI_1_DI" {
                 sd_delete_ports -sd_name ${sd_name} -port_names {SPI_1_DI}
                 sd_delete_instances -sd_name ${sd_name} -instance_names {SPI_1_DI_BIBUF}
-                sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_DI_F2M} -value {GND}
+                sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_F2M[0]} -value {GND}
             }
             "SPI_1_DO" {
                 sd_delete_ports -sd_name ${sd_name} -port_names {SPI_1_DO}
                 sd_delete_instances -sd_name ${sd_name} -instance_names {SPI_1_DO_BIBUF}
-                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_DO_M2F}
-                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_DO_OE_M2F}
+                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_M2F[0]}
+                sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_OE_M2F[0]}
             }
             default {
                 error "Pin '$name' unknown!"
@@ -52,39 +50,54 @@ auto_promote_pad_pins -promote_all 0
 
 # Add SPI_1_SS1_BIBUF instance
 sd_instantiate_macro -sd_name ${sd_name} -macro_name {BIBUF} -instance_name {SPI_1_SS1_BIBUF}
-sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_SS1_OE_M2F SPI_1_SS1_BIBUF:E}
-sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_SS_F2M SPI_1_SS1_BIBUF:Y}
-sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_SS1_M2F SPI_1_SS1_BIBUF:D}
+sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_SEL_OE_M2F SPI_1_SS1_BIBUF:E}
+sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_SEL_M2F SPI_1_SS1_BIBUF:D}
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {SPI_1_SS1_BIBUF:Y}
 sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {SPI_1_SS1_BIBUF:PAD} -port_name {SPI_1_SS1} 
 
 # Add SPI_1_CLK_BIBUF instance
 sd_instantiate_macro -sd_name ${sd_name} -macro_name {BIBUF} -instance_name {SPI_1_CLK_BIBUF}
-sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_CLK_OE_M2F SPI_1_CLK_BIBUF:E}
-sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_CLK_F2M SPI_1_CLK_BIBUF:Y}
-sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_CLK_M2F SPI_1_CLK_BIBUF:D}
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {SPI_1_CLK_BIBUF:Y}
+sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_CLK_OE_M2F SPI_1_CLK_BIBUF:E}
+sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_CLK_M2F SPI_1_CLK_BIBUF:D}
 sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {SPI_1_CLK_BIBUF:PAD} -port_name {SPI_1_CLK} 
+
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {PF_SOC_MSS:QSPI_DATA_F2M} -pin_slices {[3] [2] [1] [0]}
 
 # Add SPI_1_DI_BIBUF instance
 sd_instantiate_macro -sd_name ${sd_name} -macro_name {BIBUF} -instance_name {SPI_1_DI_BIBUF}
-sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_DI_F2M SPI_1_DI_BIBUF:Y}
+sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_F2M[0] SPI_1_DI_BIBUF:Y}
 sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {SPI_1_DI_BIBUF:D} -value {GND}
 sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {SPI_1_DI_BIBUF:E} -value {GND}
 sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {SPI_1_DI_BIBUF:PAD} -port_name {SPI_1_DI} 
 
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {PF_SOC_MSS:QSPI_DATA_M2F} -pin_slices {[3] [2] [1] [0]}
+sd_create_pin_slices -sd_name ${sd_name} -pin_name {PF_SOC_MSS:QSPI_DATA_OE_M2F} -pin_slices {[3] [2] [1] [0]}
+
 # Add SPI_1_DO_BIBUF instance
 sd_instantiate_macro -sd_name ${sd_name} -macro_name {BIBUF} -instance_name {SPI_1_DO_BIBUF}
 sd_mark_pins_unused -sd_name ${sd_name} -pin_names {SPI_1_DO_BIBUF:Y}
-sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_DO_M2F SPI_1_DO_BIBUF:D}
-sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_DO_OE_M2F SPI_1_DO_BIBUF:E}
+sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_M2F[0] SPI_1_DO_BIBUF:D}
+sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_OE_M2F[0] SPI_1_DO_BIBUF:E}
 sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {SPI_1_DO_BIBUF:PAD} -port_name {SPI_1_DO} 
 
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {PF_SOC_MSS:GPIO_0_12_OUT} -port_name {SD_CARD_CS}
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {PF_SOC_MSS:QSPI_SS0} -port_name {ADC_CSn}
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {PF_SOC_MSS:QSPI_CLK} -port_name {ADC_SCK}
-sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {PF_SOC_MSS:QSPI_DATA0} -port_name {ADC_MOSI}
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {PF_SOC_MSS:GPIO_0_12} -port_name {SD_CARD_CS}
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {PF_SOC_MSS:SPI_1_SS0} -port_name {ADC_CSn}
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {PF_SOC_MSS:SPI_1_CLK} -port_name {ADC_SCK}
+sd_connect_pin_to_port -sd_name ${sd_name} -pin_name {PF_SOC_MSS:SPI_1_DO} -port_name {ADC_MOSI}
 sd_create_scalar_port -sd_name ${sd_name} -port_name {ADC_MISO} -port_direction {INOUT}
-sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA1 ADC_MISO}
+sd_connect_pins -sd_name ${sd_name} -pin_names {PF_SOC_MSS:SPI_1_DI ADC_MISO}
 sd_connect_pins -sd_name ${sd_name} -pin_names {"ADC_IRQn" "PF_SOC_MSS:GPIO_1_20_IN" }
+sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_F2M[3]} -value {GND}
+sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_F2M[2]} -value {GND}
+sd_connect_pins_to_constant -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_F2M[1]} -value {GND}
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_M2F[3]}
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_M2F[2]}
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_M2F[1]}
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_OE_M2F[3]}
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_OE_M2F[2]}
+sd_mark_pins_unused -sd_name ${sd_name} -pin_names {PF_SOC_MSS:QSPI_DATA_OE_M2F[1]}
+
 
 
 
