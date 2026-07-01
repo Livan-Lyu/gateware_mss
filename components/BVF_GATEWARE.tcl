c24633fc3a0aef6902d05924867f7881fa16fd83
Livan <124090447@link.cuhk.edu.cn>
2026-07-01 14:28:53 +0200

    migrate COREAXI4INTERCONNECT to v3 naming for Libero v2025.1
    - Rename MASTER/SLAVE -> INITIATOR/TARGET in FIC0_INITIATOR.tcl
    - Rename AXI4mslave -> AXI4mtarget in axi_stop_cap.tcl regex
    - Remove auto-derived params (READ_INTERLEAVE, CROSSBAR_MODE, NUM_THREADS, etc.)
    - Reorder CAPE/M2 source order in BVF_GATEWARE.tcl to fix P9 port conflict

--- components/BVF_GATEWARE.tcl
+++ components/BVF_GATEWARE.tcl
@@ -263,8 +263,8 @@ sd_mark_pins_unused -sd_name ${sd_name} -pin_names {BVF_RISCV_SUBSYSTEM:CAN_1_TX
 
 source [file join $INITIAL_DIRECTORY "sources" "MSS" "components" "DRAM" $mss_ddr "MSS_DDR_IN_GATEWARE.tcl"]
 source script_support/components/SYZYGY/$syzygy_option/ADD_HIGH_SPEED_CONNECTOR.tcl 
-source script_support/components/CAPE/$cape_option/ADD_CAPE.tcl 
-source script_support/components/M2/$m2_option/ADD_M2_INTERFACE.tcl 
+source script_support/components/M2/$m2_option/ADD_M2_INTERFACE.tcl
+source script_support/components/CAPE/$cape_option/ADD_CAPE.tcl
 source script_support/components/MIPI_CSI/$mipi_csi_option/ADD_MIPI_CSI_INTERFACE.tcl
 source script_support/components/APU/$apu_option/ADD_APU.tcl 
 
